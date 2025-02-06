a3a43ca9a3075e5bdb370ce792db2f7d075f4909
Evgeny Gorelik <evgeny.gorelik@gt-arc.com>
2025-02-06 23:03:59 +0100

    added nuscenes devkit

---
+++ training/nuscenes-devkit/python-sdk/nuscenes/eval/detection/config.py
@@ -0,0 +1,32 @@
+# nuScenes dev-kit.
+# Code written by Holger Caesar, 2019.
+
+import json
+import os
+
+from nuscenes.eval.detection.data_classes import DetectionConfig
+
+
+def config_factory(configuration_name: str) -> DetectionConfig:
+    """
+    Creates a DetectionConfig instance that can be used to initialize a NuScenesEval instance.
+    Note that this only works if the config file is located in the nuscenes/eval/detection/configs folder.
+    :param configuration_name: Name of desired configuration in eval_detection_configs.
+    :return: DetectionConfig instance.
+    """
+
+    # Check if config exists.
+    if os.path.exists(os.path.join('eval_configs', '%s.json' % configuration_name)):
+        cfg_path = os.path.join('eval_configs', '%s.json' % configuration_name)
+    else:
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        cfg_path = os.path.join(this_dir, 'configs', '%s.json' % configuration_name)
+    assert os.path.exists(cfg_path), \
+        'Requested unknown configuration {}'.format(configuration_name)
+
+    # Load config file and deserialize it.
+    with open(cfg_path, 'r') as f:
+        data = json.load(f)
+    cfg = DetectionConfig.deserialize(data)
+
+    return cfg
